f1955d6811fd58e2ce61e391a07becba1f6408ae
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-09-12 19:53:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
